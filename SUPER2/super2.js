61208f0ecad2ff0e85ae3725e672d9fdd56332c8
AlmanzaGauss <almanza010893@gmail.com>
2016-05-10 00:12:26 -0500

    Firstcommit

---
+++ SUPER2/super2.js
@@ -0,0 +1,36 @@
+var express=require('express');
+var app = require('express')();
+var app2=express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set("view engine","jade");
+app.use(express.static("public"));
+
+app.get('/prueba',function(req,res){
+res.write("Prueba test");
+res.end();
+});
+
+app.get('/', function(req, res){
+  //res.sendFile(__dirname + '/index.html');
+  res.render("index");
+});
+
+app.get('/dos', function(req, res){
+  res.sendFile(__dirname + '/index2.html');
+});
+
+io.on('connection', function(socket){
+
+	console.log("contectado");
+
+  socket.on('chat message', function(msg){
+  	console.log(msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(4000, function(){
+  console.log('listening on *:3000');
+});
